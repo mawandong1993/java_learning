19ad64b4282394da9b95ab05a4cf80885826b965
mawandong1993 <mawandong1993@foxmail.com>
2019-03-20 23:38:59 +0800

    代码更新 更新依赖

---
+++ tools/java_study/src/test/java/java8/Lambda1.java
@@ -0,0 +1,28 @@
+package java8;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author mawandong
+ * @date 2019/3/20 22:11
+ */
+public class Lambda1 {
+
+    @Test
+    public void test01() {
+
+
+        Runnable r1 = () -> System.out.println("xxx");
+
+
+        List<String> list = Arrays.asList("x", "xxxx");
+
+        list.forEach(System.out::println);
+
+
+    }
+
+}
