5a83121be336853689d1cc1737178707819858b0
mawandong1993 <mawandong1993@foxmail.com>
2019-08-09 22:58:14 +0800

    调整依赖版本

---
+++ tools/java_study/src/main/java/com/learning/vo/student.java
@@ -0,0 +1,15 @@
+package com.learning.vo;
+
+import lombok.Data;
+
+/**
+ * @author mawandong
+ * @date 2019/8/9 22:55
+ */
+@Data
+public class student {
+
+    private String name;
+    private Integer age;
+
+}
